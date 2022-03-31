cfc6a78feaf33ca503e8b17e082f65c84fd09315
JoshuaLeoSmith <joshualeosmith1@gmail.com>
2022-03-31 12:20:47 -0400

    added alerts to register page

--- NoiseWater-frontend/src/app/app.component.ts
+++ NoiseWater-frontend/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+  public isLoggedIn: boolean = false;
+  username: string = '';
+  updateUserData(username: string): void {
+    this.username = username;
+  }
+
+  signOut(): void {
+
+    window.location.reload();
+    // reloading the window clears the session storage and log out the user
+
+  }
 }
